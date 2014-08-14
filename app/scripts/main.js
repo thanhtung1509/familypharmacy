95e83292d73009f241061a98df833b53ac7ac405
thanhtung1509 <thanhtung1509@gmail.com>
2014-08-14 09:21:19 +0700

    + Add font awesome to project. + Add body and footer background image + Define some layout's scss file.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,63 @@
 /*jshint unused: vars */
 require.config({
-  paths: {},
+  paths: {
+    angular: '../../bower_components/angular/angular',
+    'angular-animate': '../../bower_components/angular-animate/angular-animate',
+    'angular-cookies': '../../bower_components/angular-cookies/angular-cookies',
+    'angular-mocks': '../../bower_components/angular-mocks/angular-mocks',
+    'angular-resource': '../../bower_components/angular-resource/angular-resource',
+    'angular-route': '../../bower_components/angular-route/angular-route',
+    'angular-sanitize': '../../bower_components/angular-sanitize/angular-sanitize',
+    'angular-scenario': '../../bower_components/angular-scenario/angular-scenario',
+    'angular-touch': '../../bower_components/angular-touch/angular-touch',
+    affix: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/affix',
+    alert: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert',
+    button: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/button',
+    carousel: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/carousel',
+    collapse: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/collapse',
+    dropdown: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown',
+    tab: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tab',
+    transition: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/transition',
+    scrollspy: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/scrollspy',
+    modal: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/modal',
+    tooltip: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tooltip',
+    popover: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/popover',
+    'font-awesome': '../../bower_components/font-awesome/fonts/*'
+  },
   shim: {
-    'angular' : {'exports' : 'angular'},
-    'angular-route': ['angular'],
-    'angular-cookies': ['angular'],
-    'angular-sanitize': ['angular'],
-    'angular-resource': ['angular'],
-    'angular-animate': ['angular'],
-    'angular-touch': ['angular'],
+    angular: {
+      exports: 'angular'
+    },
+    'angular-route': [
+      'angular'
+    ],
+    'angular-cookies': [
+      'angular'
+    ],
+    'angular-sanitize': [
+      'angular'
+    ],
+    'angular-resource': [
+      'angular'
+    ],
+    'angular-animate': [
+      'angular'
+    ],
+    'angular-touch': [
+      'angular'
+    ],
     'angular-mocks': {
-      deps:['angular'],
-      'exports':'angular.mock'
+      deps: [
+        'angular'
+      ],
+      exports: 'angular.mock'
     }
   },
   priority: [
     'angular'
+  ],
+  packages: [
+
   ]
 });
 
--- test/test-main.js
+++ test/test-main.js
@@ -13,8 +13,29 @@ requirejs.config({
     baseUrl: '/base/app/scripts',
 
     paths: {
-
-    },
+    angular: '../../bower_components/angular/angular',
+    'angular-animate': '../../bower_components/angular-animate/angular-animate',
+    'angular-cookies': '../../bower_components/angular-cookies/angular-cookies',
+    'angular-mocks': '../../bower_components/angular-mocks/angular-mocks',
+    'angular-resource': '../../bower_components/angular-resource/angular-resource',
+    'angular-route': '../../bower_components/angular-route/angular-route',
+    'angular-sanitize': '../../bower_components/angular-sanitize/angular-sanitize',
+    'angular-scenario': '../../bower_components/angular-scenario/angular-scenario',
+    'angular-touch': '../../bower_components/angular-touch/angular-touch',
+    affix: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/affix',
+    alert: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert',
+    button: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/button',
+    carousel: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/carousel',
+    collapse: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/collapse',
+    dropdown: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown',
+    tab: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tab',
+    transition: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/transition',
+    scrollspy: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/scrollspy',
+    modal: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/modal',
+    tooltip: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tooltip',
+    popover: '../../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/popover',
+    'font-awesome': '../../bower_components/font-awesome/fonts/*'
+  },
 
     shim: {
         'angular' : {'exports' : 'angular'},
